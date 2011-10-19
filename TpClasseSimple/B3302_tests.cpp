121d3db694f1c232e9f6633becdb374b8bb018f2
rgicquel <rgicquel@7458c7e5-4b45-417e-a2e6-0702fdc7b5b7>
2011-10-19 23:51:49 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.tpclassesimple@6 7458c7e5-4b45-417e-a2e6-0702fdc7b5b7

--- TpClasseSimple/B3302_tests.cpp
+++ TpClasseSimple/B3302_tests.cpp
@@ -57,61 +57,61 @@ int main ( )
 //
 //	IntervalSet inter3 = inter1.Intersection ( inter2 );
 
-	inter1.Add ( -6, -4);
-	inter1.Add ( -2, 3);
-	inter1.Add ( 7, 8 );
-	inter1.Add ( 12, 18 );
-	inter1.Add ( 23, 28 );
-	inter1.Add ( 58, 63 );
+//	inter1.Add ( -6, -4);
+//	inter1.Add ( -2, 3);
+//	inter1.Add ( 7, 8 );
+//	inter1.Add ( 12, 18 );
+//	inter1.Add ( 23, 28 );
+//	inter1.Add ( 58, 63 );
+//
+//	inter2.Add ( 0, 1 );
+//	inter2.Add ( 5, 20 );
+//	inter2.Add ( 27, 45 );
+//	inter2.Add ( 48, 52 );
+//	inter2.Add ( 55, 60 );
+//
+//	IntervalSet inter3 = inter1.Intersection(inter2);
+//
+//	inter3.Display();
 
-	inter2.Add ( 0, 1 );
-	inter2.Add ( 5, 20 );
-	inter2.Add ( 27, 45 );
-	inter2.Add ( 48, 52 );
-	inter2.Add ( 55, 60 );
+	int val, val2;
+	time_t debut, fin;
 
-	IntervalSet inter3 = inter1.Intersection(inter2);
+	srand ( time ( NULL ) );
 
-	inter3.Display();
+	for ( int i = 1; i <= 100000; i++ )
+	{
+		val = (rand()/((RAND_MAX+1)/2.0)-1)*RAND_MAX*100;
+		val2 = (rand()/((RAND_MAX+1)/2.0)-1)*RAND_MAX*100;
+		inter1.Add ( val >= val2 ? val2 : val, val >= val2 ? val : val2 );
 
-//	int val, val2;
-//	time_t debut, fin;
-//
-//	srand ( time ( NULL ) );
-//
-//	for ( int i = 1; i <= 10000; i++ )
-//	{
-//		val = rand() % ((i+2)*10 - i*10) - i*10 + 1;
-//		val2 = rand() % ((i+2)*10 - i*10) - i*10 + 1;
-//		inter1.Add ( val >= val2 ? val2 : val, val >= val2 ? val : val2 );
-//
-//		val = rand ( ) % ((i+2)*10 - i*10) - i*10 + 1;
-//		val2 = rand ( ) % ((i+2)*10 - i*10) - i*10 + 1;
-//		inter2.Add ( val >= val2 ? val2 : val, val >= val2 ? val : val2 );
-//
-//		//cout << "Itération n° : " << i << endl;
-//	}
-//
-//	cout << "Nb d'éléments de inter1 : " << inter1.Count ( ) << endl;
-//	cout << "Nb d'éléments de inter2 : " << inter2.Count ( ) << endl;
-//
-//	debut = time ( NULL );
-//	IntervalSet inter3 = inter1.Union ( inter2 );
-//	fin = time ( NULL );
-//
-//	cout << "Nb d'élements de inter3 : " << inter3.Count ( ) << endl;
-//
-//	cout << "Temps d'exécution de l'union : " << difftime ( fin, debut )
-//	        << " secondes" << endl;
-//
-//	debut = time ( NULL );
-//	IntervalSet inter4 = inter1.Intersection ( inter2 );
-//	fin = time ( NULL );
-//
-//	cout << "Nb d'élements de inter4 : " << inter4.Count ( ) << endl;
-//
-//	cout << "Temps d'exécution de l'intersection : " << difftime ( fin, debut )
-//	        << " secondes" << endl;
+		val = (rand()/((RAND_MAX+1)/2.0)-1)*RAND_MAX*100;
+		val2 = (rand()/((RAND_MAX+1)/2.0)-1)*RAND_MAX*100;
+		inter2.Add ( val >= val2 ? val2 : val, val >= val2 ? val : val2 );
+
+		//cout << "Itération n° : " << i << endl;
+	}
+
+	cout << "Nb d'éléments de inter1 : " << inter1.Count ( ) << endl;
+	cout << "Nb d'éléments de inter2 : " << inter2.Count ( ) << endl;
+
+	debut = time ( NULL );
+	IntervalSet inter3 = inter1.Union ( inter2 );
+	fin = time ( NULL );
+
+	cout << "Nb d'élements de inter3 : " << inter3.Count ( ) << endl;
+
+	cout << "Temps d'exécution de l'union : " << difftime ( fin, debut )
+	        << " secondes" << endl;
+
+	debut = time ( NULL );
+	IntervalSet inter4 = inter1.Intersection ( inter2 );
+	fin = time ( NULL );
+
+	cout << "Nb d'élements de inter4 : " << inter4.Count ( ) << endl;
+
+	cout << "Temps d'exécution de l'intersection : " << difftime ( fin, debut )
+	        << " secondes" << endl;
 
 	return 1;
 
